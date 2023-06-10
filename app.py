0c103694cca86c6d1dcef04f4c5f1e8215877aeb
UseLEss213 <134964492+UseLEss213@users.noreply.github.com>
2023-06-10 18:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,81 @@
-from flask import Flask, render_template, request
-import subprocess
-import chatbot_logic
+import streamlit as st
+import requests
+import io
+from PIL import Image
+import visual_content
 
+def get_wikipedia_summary(page_title):
+    try:
+        # Make a request to the Wikipedia API
+        url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{page_title}"
+        response = requests.get(url)
 
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            return data["extract"]
+        else:
+            return "Page not found."
 
-app = Flask(__name__)
+    except requests.exceptions.RequestException:
+        return "An error occurred while fetching data from Wikipedia."
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    except KeyError:
+        return "An unexpected error occurred."
 
-@app.route('/process_input', methods=['POST'])
-def process_input():
-    user_input = request.form['user_input']
-    if user_input.lower() == "quit":
-        return "ChatBot: Goodbye!"
-    else:
-        response = chatbot_logic.get_chatbot_response(user_input)
-        return "ChatBot: " + response
+    except Exception as e:
+        return str(e)
+
+def get_visual_content(page_title):
+    try:
+        urls = visual_content.retrieve_visual_content(page_title)
+        return urls
+    except Exception as e:
+        return str(e)
+
+def download_image(image_url):
+    try:
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            image_data = response.content
+            image = Image.open(io.BytesIO(image_data))
+            return image
+        else:
+            return None
+    except requests.exceptions.RequestException:
+        return None
+    except Exception as e:
+        return None
+
+def display_image(image):
+    # Resize the image if necessary
+    max_width = 300
+    if image.width > max_width:
+        ratio = max_width / float(image.width)
+        height = int((float(image.height) * float(ratio)))
+        image = image.resize((max_width, height), Image.ANTIALIAS)
+
+    # Display the image
+    st.image(image, use_column_width=True)
+
+def main():
+    st.title("Wikipedia ChatBot")
+
+    user_input = st.text_input("User Input")
+    if st.button("Send"):
+        if user_input.lower() == "quit":
+            st.write("ChatBot: Goodbye!")
+        else:
+            summary = get_wikipedia_summary(user_input)
+            visual_urls = get_visual_content(user_input)
+
+            st.write(f"ChatBot: {summary}")
+
+            if visual_urls:
+                image_url = visual_urls[0]  # Assuming the first URL is the desired image
+                image = download_image(image_url)
+                if image:
+                    display_image(image)
 
 if __name__ == '__main__':
-    app.run()
+    main()
