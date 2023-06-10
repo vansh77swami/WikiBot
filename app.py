0a70e885d9c70dd944c5d992ede1ce92368ff138
UseLEss213 <134964492+UseLEss213@users.noreply.github.com>
2023-06-10 17:50:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 import requests
-import io
+from bs4 import BeautifulSoup
 from PIL import Image
-import visual_content
-
+import io
 
 def get_wikipedia_summary(page_title):
     try:
-        # Make a request to the Wikipedia API
         url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{page_title}"
         response = requests.get(url)
 
-        # Check if the request was successful
         if response.status_code == 200:
             data = response.json()
             return data["extract"]
@@ -27,14 +24,24 @@ def get_wikipedia_summary(page_title):
     except Exception as e:
         return str(e)
 
-
-def get_visual_content(page_title):
+def retrieve_visual_content(page_title):
     try:
-        urls = visual_content.retrieve_visual_content(page_title)
-        return urls
-    except Exception as e:
-        return str(e)
+        url = f"https://en.wikipedia.org/wiki/{page_title}"
+        response = requests.get(url)
 
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            image_tags = soup.find_all("img")
+            urls = [img["src"] for img in image_tags]
+            return urls
+        else:
+            return []
+
+    except requests.exceptions.RequestException:
+        return []
+
+    except Exception as e:
+        return []
 
 def download_image(image_url):
     try:
@@ -50,42 +57,31 @@ def download_image(image_url):
     except Exception as e:
         return None
 
-
 def display_image(image):
-    # Resize the image if necessary
     max_width = 300
     if image.width > max_width:
         ratio = max_width / float(image.width)
         height = int((float(image.height) * float(ratio)))
         image = image.resize((max_width, height), Image.ANTIALIAS)
-
-    # Display the image
     st.image(image)
-
 
 def main():
     st.title("Wikipedia ChatBot")
 
-    user_input = st.text_input("Enter a topic")
+    user_input = st.text_input("Enter your query")
 
     if user_input.lower() == "quit":
         st.write("ChatBot: Goodbye!")
     else:
         summary = get_wikipedia_summary(user_input)
-        visual_urls = get_visual_content(user_input)
-
+        visual_urls = retrieve_visual_content(user_input)
         st.write("ChatBot:", summary)
 
         if visual_urls:
-            image_url = visual_urls[0]  # Assuming the first URL is the desired image
+            image_url = visual_urls[0]
             image = download_image(image_url)
             if image:
                 display_image(image)
-
-        with open("index.html", "r", encoding="utf-8") as file:
-            html_code = file.read()
-            st.markdown(html_code, unsafe_allow_html=True)
-
 
 if __name__ == '__main__':
     main()
