1bb6d20971373cb58cf6d2cc494114f9fa381822
UseLEss213 <134964492+UseLEss213@users.noreply.github.com>
2023-06-10 17:45:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,9 +82,20 @@ def main():
             if image:
                 display_image(image)
 
-        with open("index.html", "r", encoding="utf-8") as file:
-            html_code = file.read()
-            st.markdown(html_code, unsafe_allow_html=True)
+        st.markdown("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Wikipedia ChatBot</title>
+            <style>
+                /* Add your CSS styles here */
+            </style>
+        </head>
+        <body>
+            <!-- Add your HTML content here -->
+        </body>
+        </html>
+        """, unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
