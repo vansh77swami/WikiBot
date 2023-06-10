b6d0f9a3783c8af886692e70a41b0cbb0a944b18
UseLEss213 <134964492+UseLEss213@users.noreply.github.com>
2023-06-10 23:45:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import requests
-import io
-from PIL import Image
-import visual_content
 
 def get_wikipedia_summary(page_title):
     try:
@@ -26,61 +23,26 @@ def get_wikipedia_summary(page_title):
     except Exception as e:
         return str(e)
 
-def get_visual_content(page_title):
-    try:
-        urls = visual_content.retrieve_visual_content(page_title)
-        return urls
-    except Exception as e:
-        return str(e)
-
-def download_image(image_url):
-    try:
-        response = requests.get(image_url)
-        if response.status_code == 200:
-            image_data = response.content
-            image = Image.open(io.BytesIO(image_data))
-            return image
-        else:
-            return None
-    except requests.exceptions.RequestException:
-        return None
-    except Exception as e:
-        return None
-
-def display_image(image):
-    # Resize the image if necessary
-    max_width = 300
-    if image.width > max_width:
-        ratio = max_width / float(image.width)
-        height = int((float(image.height) * float(ratio)))
-        image = image.resize((max_width, height), Image.ANTIALIAS)
-
-    # Display the image
-    st.image(image, use_column_width=True)
-
 def main():
     st.title("WikiBot")
 
     user_input = st.text_input("User Input", key="user_input")
 
-    if st.button("Send") or (st.session_state.user_input and st.session_state.user_input[-1] == '\n'):
-        st.session_state.user_input = st.session_state.user_input.rstrip('\n')
-        if st.session_state.user_input.lower() == "quit":
+    # Check if Enter key was pressed or Send button was clicked
+    enter_pressed = st.button("Send") or (st.session_state.user_input and st.session_state.user_input[-1] == '\n')
+    
+    if enter_pressed:
+        # Remove the newline character from user input
+        user_input = st.session_state.user_input.rstrip('\n')
+        
+        if user_input.lower() == "quit":
             st.write("WikiBot: Goodbye!")
         else:
-            summary = get_wikipedia_summary(st.session_state.user_input)
-            visual_urls = get_visual_content(st.session_state.user_input)
-
+            summary = get_wikipedia_summary(user_input)
             st.write(f"WikiBot: {summary}")
 
-            if visual_urls:
-                for image_url in visual_urls:
-                    image = download_image(image_url)
-                    if image:
-                        display_image(image)
-
     # Clear the input field after sending the message
-    if st.session_state.user_input and st.session_state.user_input[-1] == '\n':
+    if enter_pressed:
         st.session_state.user_input = ''
 
 if __name__ == '__main__':
