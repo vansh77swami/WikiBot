2bb4e213af30562bf7e6be74d4834cecc021a923
UseLEss213 <134964492+UseLEss213@users.noreply.github.com>
2023-06-10 18:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-import streamlit as st
+from flask import Flask, render_template, request
 import subprocess
 import chatbot_logic
 
-def main():
-    st.title("ChatBot")
-    user_input = st.text_input("User Input")
 
-    if st.button("Submit"):
-        if user_input.lower() == "quit":
-            st.write("ChatBot: Goodbye!")
-        else:
-            response = chatbot_logic.get_chatbot_response(user_input)
-            st.write("ChatBot: " + response)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/process_input', methods=['POST'])
+def process_input():
+    user_input = request.form['user_input']
+    if user_input.lower() == "quit":
+        return "ChatBot: Goodbye!"
+    else:
+        response = chatbot_logic.get_chatbot_response(user_input)
+        return "ChatBot: " + response
 
 if __name__ == '__main__':
-    main()
+    app.run()
